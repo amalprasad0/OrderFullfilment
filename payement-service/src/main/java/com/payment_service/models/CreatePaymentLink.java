9362dfc43f03d407db0a08c5def173a6fe4bab51
Amal Prasad <78908990+amalprasad0@users.noreply.github.com>
2025-04-11 09:40:52 +0530

    fix(CreatePaymentLink): Correct field naming conventions for productName and currency

--- payement-service/src/main/java/com/payment_service/models/CreatePaymentLink.java
+++ payement-service/src/main/java/com/payment_service/models/CreatePaymentLink.java
@@ -1,8 +1,17 @@
 package com.payment_service.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreatePaymentLink {
-    public String ProductName;
+    public String productName;
     public float unit_amount;
-    public String Currency ;
-    
+    public String currency ;
+    public int quantity;
 }
